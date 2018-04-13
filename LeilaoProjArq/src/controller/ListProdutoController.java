c2754631a2df6010201e37d05e66c217a54b9889
Hercílio <hercilio.ortiz@acad.pucrs.br>
2018-04-13 02:20:55 -0300

    Menu do Leilão e Lista de produtos finalizados

---
+++ LeilaoProjArq/src/controller/ListProdutoController.java
@@ -0,0 +1,84 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ProdutoDAO;
+import model.Produto;
+
+/**
+ * Servlet implementation class ListProdutoController
+ */
+@WebServlet("/ListProdutoController")
+public class ListProdutoController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+    int id;
+    String nome, descricao;
+    double valorInicial;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListProdutoController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("ISO-8859-1");//CODIFICAO charset=ISO-8859-1 PARA ACENTUACAO E CARACTRES ESPECIAIS
+		
+		ProdutoDAO dao = new ProdutoDAO();   
+        ArrayList<Produto> listProdutos = dao.getProdutos();
+        
+        response.setContentType("text/html;charset=UTF-8");
+        ServletOutputStream out = response.getOutputStream();
+	    
+        out.println("<html>");
+        out.println("<head><title>Listas de Produtos</title></head>\n");
+        out.println("<body>");
+        
+        out.println("<h2>Lista de Produtos em Leilao</h2>\r\n" + 
+        		"<p>Selecione o produto desejado e precione o botao \"Fazer Lances\"</p>");
+        out.println("<br>");
+
+	    out.println("<form action=\"/LancesController\">");
+	    out.println("<input list=\"produtos\" name=\"produtos\">");
+	    out.println("<datalist id=\"produtos\">");
+	    
+	    for(Produto x : listProdutos) {	    	
+	    	out.println("<option value=" + x.getNome() + ">" + x.getId() + "</option>" );
+	    }
+	   
+	    out.println("</datalist>");
+	    out.println("<br><br>");
+	    out.println("<input type=\"submit\" value=\"Fazer Lances\">");
+	    
+	    out.println("</form>");
+	    
+	    out.println("</body></html>");
+	    
+	    out.close();
+	    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- LeilaoProjArq/src/model/ProdutoDAO.java
+++ LeilaoProjArq/src/model/ProdutoDAO.java
@@ -7,6 +7,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
 import model.Produto;
 
 public class ProdutoDAO {
@@ -57,6 +60,61 @@ public class ProdutoDAO {
             }
         }
         return sucesso;
+    }
+    
+    public ArrayList<Produto> getProdutos() {
+
+    	ArrayList<Produto> listProduto = null;
+    	cmd = "SELECT * FROM PRODUTO;";
+    	
+
+    	try {
+    		connection = new ConnectionFactory().getConnection();    		
+    		System.out.println("Conexão aberta!");
+    		connection.setAutoCommit(false);
+    		
+            ps = connection.prepareStatement(cmd);
+            ResultSet rs = ps.executeQuery();
+    
+            
+            listProduto = new ArrayList<>();
+            
+            while(rs.next()) {  
+      
+            	int id = rs.getInt("id_produto");
+                String nome = rs.getString("nome");
+                int valorInicial = rs.getInt("valor_inicial");
+                int maiorLance = rs.getInt("maiorLance");
+                String descricao = rs.getString("descricao");
+       
+                Produto prod = new Produto();
+                prod.setId(id);
+                prod.setNome(nome);  
+                prod.setValorInicial(valorInicial);
+                prod.setMaiorLance(maiorLance);
+                prod.setDescricao(descricao);
+               
+                listProduto.add(prod);  
+            }
+            
+            ps.close();
+            connection.close();
+            
+
+        } catch (SQLException ex) {
+            try {
+                System.out.print("Não foi possível listar os dados");
+                connection.rollback();
+            } catch (Exception ex2) {
+            }
+        } finally {
+            try {
+                ps.close();
+                connection.close();
+            } catch (Exception ex3) {
+            }
+        }
+        return listProduto;
     }
 
     public boolean excluir(Produto prod) {
